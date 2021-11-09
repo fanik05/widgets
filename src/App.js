677931ad700a614a78130c6a8f1ffeb5de12d69b
fanik05 <fatinanik129@gmail.com>
2021-11-09 14:33:40 +0600

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 
 const items = [
     {
@@ -19,44 +21,44 @@ const items = [
     }
 ]
 
-// const options = [
-//     {
-//         label: 'The Color Red',
-//         value: 'red'
-//     },
-//     {
-//         label: 'The Color Green',
-//         value: 'green'
-//     },
-//     {
-//         label: 'A Shade of Blue',
-//         value: 'blue'
-//     },
-// ]
-
-const showAccordion = () => {
-    if (window.location.pathname === '/') return <Accordion items={items} />
-}
-  
-const showList = () => {
-    if (window.location.pathname === '/list') return <Search />
-}
-  
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') return <Dropdown />
-}
-  
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') return <Translate />
-}
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    },
+]
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0])
+
     return (
         <>
-            {showAccordion}
-            {showList}
-            {showDropdown}
-            {showTranslate}
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </>
     )
 }
---
+++ src/components/Link.js
@@ -0,0 +1,19 @@
+const Link = ({ className, href, children }) => {
+    const onClick = event => {
+        if (event.metaKey || event.ctrlKey) return
+
+        event.preventDefault()
+        window.history.pushState({}, '', href)
+
+        const navEvent = new PopStateEvent('popstate')
+        window.dispatchEvent(navEvent)
+    }
+ 
+    return (
+        <a className={className} href={href} onClick={onClick}>
+            {children}
+        </a>
+    )
+}
+
+export default Link
